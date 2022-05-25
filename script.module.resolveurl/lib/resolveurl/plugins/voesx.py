81181d2108ac3fc65446e0c80a8e23cc95192753
DWH-WFC <45105289+DWH-WFC@users.noreply.github.com>
2022-05-25 16:23:39 +0200

    no message

--- script.module.resolveurl/lib/resolveurl/plugins/voesx.py
+++ script.module.resolveurl/lib/resolveurl/plugins/voesx.py
@@ -24,9 +24,15 @@ from resolveurl.resolver import ResolveUrl, ResolverError
 
 
 class VoeResolver(ResolveUrl):
+<<<<<<< HEAD
     name = 'Voe'
     domains = ['voe.sx', 'voe-unblock.com', 'voe-unblock.net', 'voeunblock.com']
     pattern = r'(?://|\.)(voe(?:-?unblock)?\.(?:sx|com|net))/(?:e/)?([0-9A-Za-z]+)'
+=======
+    name = "Voe"
+    domains = ["voe.sx", "voe-unblock.com"]
+    pattern = r'(?://|\.)(voe(?:-unblock)?\.(?:sx|com))/(?:e/)?([0-9A-Za-z]+)'
+>>>>>>> parent of 20306c0 (Update VOE Hoster)
 
     def get_media_url(self, host, media_id):
         web_url = self.get_url(host, media_id)
