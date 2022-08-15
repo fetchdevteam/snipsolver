111b302cdcb19f774ad9f3ae1ade0c3c75d96d46
DWH-WFC <45105289+DWH-WFC@users.noreply.github.com>
2022-08-15 08:36:49 +0200

    VOE fix

--- script.module.resolveurl/lib/resolveurl/plugins/neohd.py
+++ script.module.resolveurl/lib/resolveurl/plugins/neohd.py
@@ -1,6 +1,6 @@
 """
-    Plugin for ResolveUrl
-    Copyright (C) 2022 shellc0de
+    Plugin for ResolveURL
+    Copyright (C) 2022 gujal
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -20,17 +20,17 @@ from resolveurl.plugins.__resolve_generic__ import ResolveGeneric
 from resolveurl.lib import helpers
 
 
-class FileMoonResolver(ResolveGeneric):
-    name = 'FileMoon'
-    domains = ['filemoon.sx', 'filemoon.to']
-    pattern = r'(?://|\.)(filemoon\.(?:sx|to))/(?:e|d)/([0-9a-zA-Z]+)'
+class NeoHDResolver(ResolveGeneric):
+    name = 'NeoHD'
+    domains = ['neohd.xyz']
+    pattern = r'(?://|\.)(neohd\.xyz)/embed/([0-9a-f-]+)'
 
     def get_media_url(self, host, media_id):
         return helpers.get_media_url(
             self.get_url(host, media_id),
-            patterns=[r'''sources:\s*\[{file:\s*["'](?P<url>[^"']+)'''],
+            patterns=[r'''"file":"(?P<url>[^"]+).+?"label":"(?P<label>[^"]+)'''],
             generic_patterns=False
-        )
+        ).replace(' ', '%20')
 
     def get_url(self, host, media_id):
-        return self._default_get_url(host, media_id, template='https://{host}/e/{media_id}')
+        return self._default_get_url(host, media_id, template='https://{host}/embed/{media_id}')
